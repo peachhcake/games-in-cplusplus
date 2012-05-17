508dae38176e42974a625c4a2c2380dff2c846ef
Rawa Saleh <rawa.saleh@gmail.com>
2012-05-17 00:20:54 -0400

    changed maxnum and maxguesses to const

--- guessgame.cpp
+++ guessgame.cpp
@@ -11,21 +11,22 @@ using namespace std;
 
 int main()
 {
+	const int MAXNUM = 20, MAXGUESSES = 6;
+
 	string player;
 	int guessestaken = 0;
 	int number, guess;
-	int maxnum = 20, maxguesses = 6;
 	bool guessed = false;
 
 	srand(time(NULL));
-	number = (rand() % maxnum) + 1;
+	number = (rand() % MAXNUM) + 1;
 
 	cout << "Hello! What is your name?" << endl;
 	cin >> player;
-	cout << "Well, " << player << " I am thinking of a number between 1 and " << maxnum 
+	cout << "Well, " << player << " I am thinking of a number between 1 and " << MAXNUM 
 		 << "." << endl;
 
-	while ((guessestaken < maxguesses) && (guessed == false))
+	while ((guessestaken < MAXGUESSES) && (guessed == false))
 	{
 		cout << "Take a guess." << endl;
 		cin >> guess;
