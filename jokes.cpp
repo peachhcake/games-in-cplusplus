1639f12fc9ee99506569f4399e4239a7633488c5
Rawa Saleh <rawa.saleh@gmail.com>
2012-05-17 00:18:57 -0400

    Jokes

---
+++ jokes.cpp
@@ -0,0 +1,29 @@
+// Author	:	Rawa Saleh
+// Date		:	May 8, 2012
+// Compiler	:	Microsoft Visual Studio C++
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string nothing;
+
+	cout << "What do you get when you cross a snowman with a vampire?";
+	cin >> nothing;
+	cout << "Frostbite!" << endl;
+	
+	cout << "What do dentists call an astronaut's cavity?";
+	cin >> nothing;
+	cout << "A black hole!" << endl;
+
+	cout << "Knock knock.";
+	cin >> nothing;
+	cout << "Who's there?";
+	cin >> nothing;
+	cout << "Interrupting cow.";
+	cin >> nothing;
+	cout << "Interrupting cow wh" << "-MOO!" << endl;
+
+}
